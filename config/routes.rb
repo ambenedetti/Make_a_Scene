7cd4abe2f4e75433c10db788579b7cb30e751d79
Ana Maria Benedetti <ambenedetti@gmail.com>
2019-11-11 15:50:32 +0100

    added dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
     resources :bookings, except: [:destroy]
   end
   resources :images, only: [:destroy]
+  get 'dashboard', to: 'pages#dashboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
