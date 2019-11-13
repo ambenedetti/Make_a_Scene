975b65f15b86e3a37cd1d72dca03884d10f273d5
Belen Battaglini <belenbattaglini@gmail.com>
2019-11-13 10:58:50 +0100

    Fixed seed problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Image.destroy_all
 Booking.destroy_all
+Image.destroy_all
 Product.destroy_all
 User.destroy_all
 
