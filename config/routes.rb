17e47137713e9fe3e658e05588edee6f51b58a52
Belen Battaglini <belenbattaglini@gmail.com>
2019-11-13 12:43:58 +0100

    Added route for dashboard, method in PagesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'dashboard', to: 'pages#dashboard', as: :dashboard
   root to: 'pages#home'
   resources :products do
     resources :images, only: [:create, :new]
     resources :bookings, only: [:new,:create]
   end
   resources :images, only: [:destroy]
-  get 'dashboard', to: 'pages#dashboard'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
